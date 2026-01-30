3e3e3073f672227877aa93a1096aa1a62741129c
Sanjay <101442465+imsaba16@users.noreply.github.com>
2026-01-30 17:59:12 +0530

    Add new problems and fix existing logic in Day4Problems; implement largest element smaller than X, first repeating element, and index of minimum element

--- src/main/kotlin/newer/day4/Day4Problems.kt
+++ src/main/kotlin/newer/day4/Day4Problems.kt
@@ -9,7 +9,7 @@ private fun linearSearch() {
      * Given an array of integers and a target value, return the index of the first occurrence of the target.
      * If not found, return -1.
      * */
-    val arr = intArrayOf(1, 4, 2, 6, 4, 6, 7, 7, 5)
+    val arr = intArrayOf(4, 2, 6, 1, 4, 6, 7, 7, 5)
     val arrSorted = intArrayOf(1, 2, 4, 6, 6, 7, 9)
     fun problem1(arr: IntArray, target: Int) : Int {
 //         For loop implementation
@@ -35,9 +35,15 @@ private fun linearSearch() {
      * */
     fun problem2(arr: IntArray, target: Int): Int {
         var currentIndex = -1
-        for (i in 0..arr.lastIndex) {
+   /*     for (i in 0..arr.lastIndex) {
             if (arr[i] == target) {
                 currentIndex = i
+            }
+        }*/
+        //Provided suggestion
+        for (i in arr.lastIndex downTo 0) {
+            if (arr[i] == target) {
+                return i
             }
         }
         return currentIndex
@@ -70,4 +76,54 @@ private fun linearSearch() {
         return occurrences
     }
     println(problem4(arr, 6))
+
+    /**
+     * Find the largest element smaller than X in an unsorted array.
+     * */
+    fun problem5(arr: IntArray, target: Int): Int {
+//        var smallest = arr[0]
+        var smallest = Int.MIN_VALUE
+        for (i in 0..arr.lastIndex) {
+            // Bug in this problem in if condition
+//            if (target > arr[i] && smallest < target) {
+//                smallest = arr[i]
+//            }
+            if (arr[i] in (smallest + 1)..<target) {
+                smallest = arr[i]
+            }
+        }
+        return smallest
+    }
+    println(problem5(arr, 6))
+
+    /**
+     * Given an array, find the first repeating element.
+     * */
+    fun problem6(arr: IntArray) : Int {
+        var repeatedElement = -1
+        for (i in 0..arr.lastIndex) {
+            for (j in i + 1 ..arr.lastIndex) {
+                if (arr[i] == arr[j]) {
+                    repeatedElement = arr[i]
+                    return repeatedElement
+                }
+            }
+        }
+        return repeatedElement
+    }
+    println(problem6(arr))
+
+    /**
+     * Find the index of the minimum element in an array.
+     * */
+    fun problem7(arr: IntArray) : Int {
+        var index = 0
+        for (i in 1 .. arr.lastIndex) {
+            if (arr[i] < arr[index]) {
+                index = i
+            }
+        }
+        return index
+    }
+    println(problem7(arr))
 }
